274b682078f4cc011ab771d4444433f081e4c184
Vamshi Reddy <vamshiproject02@gmail.com>
2024-01-25 23:31:30 +0530

    auth project is added
    Signed-off-by: Vamshi Reddy <vamshiproject02@gmail.com>

--- read-json/target/debug/deps/read_json-5189b1634021cf3b.d
+++ read-json/target/debug/deps/read_json-5189b1634021cf3b.d
@@ -1,5 +1,5 @@
-/Users/vamshireddy/Desktop/rust-project/read-json/target/debug/deps/libread_json-5189b1634021cf3b.rmeta: src/main.rs
+/Users/vamshireddy/Documents/Vamshi-reddy/gitHub/rust-project/read-json/target/debug/deps/libread_json-5189b1634021cf3b.rmeta: src/main.rs
 
-/Users/vamshireddy/Desktop/rust-project/read-json/target/debug/deps/read_json-5189b1634021cf3b.d: src/main.rs
+/Users/vamshireddy/Documents/Vamshi-reddy/gitHub/rust-project/read-json/target/debug/deps/read_json-5189b1634021cf3b.d: src/main.rs
 
 src/main.rs:
--- rust-csv/target/debug/deps/rust_csv-9c4815175c668c2f.d
+++ rust-csv/target/debug/deps/rust_csv-9c4815175c668c2f.d
@@ -1,5 +1,5 @@
-/Users/vamshireddy/Desktop/rust-project/rust-csv/target/debug/deps/librust_csv-9c4815175c668c2f.rmeta: src/main.rs
+/Users/vamshireddy/Documents/Vamshi-reddy/gitHub/rust-project/rust-csv/target/debug/deps/librust_csv-9c4815175c668c2f.rmeta: src/main.rs
 
-/Users/vamshireddy/Desktop/rust-project/rust-csv/target/debug/deps/rust_csv-9c4815175c668c2f.d: src/main.rs
+/Users/vamshireddy/Documents/Vamshi-reddy/gitHub/rust-project/rust-csv/target/debug/deps/rust_csv-9c4815175c668c2f.d: src/main.rs
 
 src/main.rs:
